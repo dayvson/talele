0a932ad9e511147da65fced552444ae572ce2942
Maxwell Dayvson <dayvson@gmail.com>
2012-08-22 18:54:54 -0300

    Full implementation of puzzle grid with manual scroll and slide

---
+++ talele/PuzzleGrid.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Maxwell Dayvson <dayvson@gmail.com>
+ * Copyright (c) Luiz Adolpho <luiz.adolpho@gmail.com>
+ * Created 08/2012
+ * All rights reserved.
+ */
+#import "cocos2d.h"
+
+@interface PuzzleGrid : CCLayer
+{
+    int currentPage;
+	int totalPages;
+    bool isMoving;
+	bool swipeInMenu;
+	float moveDelta;
+	float distanceToNextPage;
+	float animationSpeed; // 0.0 to 1.0 slow/fast animation page.
+    CGPoint touchStarted;
+	CGPoint touchStop;
+	CGPoint padding;
+	CGPoint menuPosition;
+    CGSize screenSize;
+    CCDirector* director;
+	CCMenuItem *selectedItem;
+    tCCMenuState state;
+    id delegate;
+
+}
+@property (nonatomic, readwrite) int currentPage;
+@property (nonatomic, readwrite) int totalPages;
+@property (nonatomic, readwrite) bool isMoving;
+@property (nonatomic, readwrite) bool swipeInMenu;
+@property (nonatomic, readwrite) float moveDelta;
+@property (nonatomic, readwrite) float distanceToNextPage;
+@property (nonatomic, readwrite) CGPoint touchStarted;
+@property (nonatomic, readwrite) CGPoint touchStop;
+@property (nonatomic, readwrite) CGPoint padding;
+@property (nonatomic, readwrite) CGPoint menuPosition;
+@property (nonatomic, readwrite) float animationSpeed;
+
+-(id) initWithArray:(NSMutableArray*)items position:(CGPoint)pos padding:(CGPoint)pad;
+-(void) adjustItems;
+-(void) GoToNextPage;
+-(void) GoToPrevPage;
+-(NSObject*) getCurrentPageData;
+-(CGPoint) getCurrentPagePosition;
+-(CGPoint) getCurrentPagePositionWithOffset:(float)offset;
+-(CCMenuItem*) getItemInTouch:(UITouch*)touch;
+- (void)setDelegate:(id)delegate;
+@end
+
+@interface NSObject(PuzzleGridDelegateMethods)
+- (void)onMoveToCurrentPage:(NSObject *)puzzleData;
+@end
---
+++ talele/PuzzleGrid.m
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) Maxwell Dayvson <dayvson@gmail.com>
+ * Copyright (c) Luiz Adolpho <luiz.adolpho@gmail.com>
+ * Created 08/2012
+ * All rights reserved.
+ */
+
+#import "PuzzleGrid.h"
+
+@implementation PuzzleGrid
+
+@synthesize padding;
+@synthesize menuPosition;
+@synthesize touchStarted;
+@synthesize touchStop;
+@synthesize isMoving;
+@synthesize swipeInMenu;
+@synthesize	moveDelta;
+@synthesize distanceToNextPage;
+@synthesize totalPages;
+@synthesize currentPage;
+@synthesize animationSpeed;
+
+-(id) initWithArray:(NSMutableArray*)items position:(CGPoint)pos padding:(CGPoint)pad
+{
+	if ((self = [super init])){
+		self.isTouchEnabled = YES;
+		int z = 1;
+		for (id item in items){
+			[self addChild:item z:z tag:z];
+			++z;
+		}
+		isMoving = false;
+		swipeInMenu = false;
+		menuPosition = pos;
+		distanceToNextPage = 10;
+		animationSpeed = 1;
+		padding = pad;
+		currentPage = 0;
+        director = [CCDirector sharedDirector];
+        screenSize = [director winSize];
+		[self adjustItems];
+        self.position = menuPosition;
+
+	}
+	return self;
+}
+
+-(void) adjustItems
+{
+    int c = 0;
+    totalPages = self.children.count;
+    for (CCMenuItem* item in self.children){
+		item.position = CGPointMake(self.position.x + padding.x + (c * screenSize.width), 
+                                    self.position.y - padding.y);
+        c++;
+	}
+}
+
+-(void) addChild:(CCMenuItem*)child z:(int)z tag:(int)aTag
+{
+	return [super addChild:child z:z tag:aTag];
+}
+
+-(CCMenuItem*) getItemInTouch:(UITouch*)touch
+{
+	CGPoint touchLocation = [director convertToGL: [touch locationInView: [touch view]]];	
+	for (CCMenuItem* item in self.children){
+		CGPoint local = [item convertToNodeSpace:touchLocation];
+		CGRect area = [item rect];
+        area.origin = CGPointZero;
+		if (CGRectContainsPoint(area, local)){
+			return item;
+		}
+	}
+	return nil;
+}
+
+-(void) registerWithTouchDispatcher
+{
+	[[CCTouchDispatcher sharedDispatcher] addTargetedDelegate:self priority:INT_MIN+1 swallowsTouches:YES];
+}
+
+-(BOOL) ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	touchStarted = [director convertToGL:[touch locationInView:[touch view]]];
+	if (state != kCCMenuStateWaiting){
+		return NO;
+	}
+	selectedItem = [self getItemInTouch:touch];
+	[selectedItem selected];
+	if (!swipeInMenu || (swipeInMenu && selectedItem)){
+		state = kCCMenuStateTrackingTouch;
+		return YES;
+	}
+	return NO;
+}
+
+
+- (CGPoint) getCurrentPagePosition
+{
+	return CGPointMake(menuPosition.x - (currentPage * screenSize.width),
+                       menuPosition.y);
+}
+
+- (CGPoint) getCurrentPagePositionWithOffset:(float)offset
+{
+	return CGPointMake(menuPosition.x-(currentPage * screenSize.width)+offset, menuPosition.y);
+}
+
+
+- (void) moveToCurrentPage
+{
+	id action = [CCMoveTo actionWithDuration:(animationSpeed*0.5) position:[self getCurrentPagePosition]];
+	[self runAction:action];
+    [delegate onMoveToCurrentPage:[self getCurrentPageData]];
+}
+
+
+-(void) GoToNextPage{
+    if ((currentPage+1) < totalPages){
+        currentPage +=1;
+    }
+    [self moveToCurrentPage];
+}
+
+-(void) GoToPrevPage{
+    if (currentPage > 0){
+        currentPage -=1;
+    }
+    [self moveToCurrentPage];    
+}
+
+-(NSObject*) getCurrentPageData{
+    CCMenuItem* itemPage = [self.children objectAtIndex:currentPage];
+    return itemPage.userData;
+}
+
+- (void)setDelegate:(id)aDelegate {
+    delegate = aDelegate;
+}
+
+-(void) ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{	
+	if(isMoving){
+		isMoving = false;
+		if( totalPages > 1 && (distanceToNextPage < abs(moveDelta))){
+			bool isForward = (moveDelta < 0) ? true : false;
+			if(isForward && (currentPage+1) < totalPages){
+				currentPage++;
+			}else if(!isForward && (currentPage > 0)){
+				currentPage--;
+			}
+		}
+		[self moveToCurrentPage];			
+	}else{
+		[selectedItem unselected];
+		[selectedItem activate];
+	}
+	state = kCCMenuStateWaiting;
+}
+
+-(void) ccTouchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	[selectedItem unselected];
+	state = kCCMenuStateWaiting;
+}
+
+-(void) ccTouchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	touchStop = [director convertToGL:[touch locationInView:[touch view]]];
+	moveDelta = touchStop.x - touchStarted.x;
+	[self setPosition:[self getCurrentPagePositionWithOffset:moveDelta]];
+	isMoving = true;
+}
+
+-(void) dealloc
+{
+	[super dealloc];
+}
+
+@end
