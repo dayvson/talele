cc801e99c999c47940757f6acb3814553b1fb0e5
Maxwell Dayvson <dayvson@gmail.com>
2012-08-21 23:32:02 -0300

    Added kConstants of scenes and pieces alignment

---
+++ talele/GameConstants.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Maxwell Dayvson <dayvson@gmail.com>
+ * Copyright (c) Luiz Adolpho <luiz.adolpho@gmail.com>
+ * Created 08/2012
+ * All rights reserved.
+ */
+
+typedef enum {
+    kNoSceneUninitialized=0,
+    kMainMenu=1,
+    kPuzzleSelection=2,
+    kLevelEasy=3,
+    kLevelHard=4
+} SceneTypes;
+
+typedef enum {
+    kVAlignTOP=0,
+    kVAlignCENTER=1,
+    kVAlignBOTTOM=2,
+    kHAlignLEFT=3,
+    kHAlignCENTER=4,
+    kHAlignRIGHT=5
+} AlignPiece;
+
