6bac77368443bda09e797b9fe4317162dc92365b
Maxwell Dayvson <dayvson@gmail.com>
2012-08-21 23:34:17 -0300

    A splashscreen in this introduction layer this is a jump to main scene

---
+++ talele/IntroLayer.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Maxwell Dayvson <dayvson@gmail.com>
+ * Copyright (c) Luiz Adolpho <luiz.adolpho@gmail.com>
+ * Created 08/2012
+ * All rights reserved.
+ */
+
+#import "cocos2d.h"
+
+@interface IntroLayer : CCLayer
+{
+}
+
++(CCScene *) scene;
+
+@end
---
+++ talele/IntroLayer.m
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Maxwell Dayvson <dayvson@gmail.com>
+ * Copyright (c) Luiz Adolpho <luiz.adolpho@gmail.com>
+ * Created 08/2012
+ * All rights reserved.
+ */
+
+#import "IntroLayer.h"
+#import "GameConstants.h"
+#import "GameManager.h"
+
+@implementation IntroLayer
+
++(CCScene *) scene
+{
+	CCScene *scene=[CCScene node];
+	IntroLayer *layer = [IntroLayer node];
+	[scene addChild: layer];
+	return scene;
+}
+
+-(void) onEnter
+{
+	[super onEnter];
+	CGSize size = [[CCDirector sharedDirector] winSize];
+	CCSprite *background;
+    background = [CCSprite spriteWithFile:@"Default-Landscape~ipad.png"];
+	background.position = ccp(size.width/2, size.height/2);
+    
+	[self addChild: background];
+	[self scheduleOnce:@selector(makeTransition:) delay:2];
+}
+
+-(void) makeTransition:(ccTime)dt
+{
+    [[GameManager sharedGameManager] runSceneWithID:kMainMenu];
+}
+@end
